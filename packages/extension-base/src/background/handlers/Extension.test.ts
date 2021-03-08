d7daff997ced8609b40824a7085a712749b72c88
KarishmaBothara <bothara.karishma@gmail.com>
2021-03-08 15:16:49 +1300

    adding test for signing payload with custom signed extension

--- packages/extension-base/src/background/handlers/Extension.test.ts
+++ packages/extension-base/src/background/handlers/Extension.test.ts
@@ -3,15 +3,23 @@
 
 import '../../../../../__mocks__/chrome';
 
+import type { ExtDef } from '@polkadot/types/extrinsic/signedExtensions/types';
 import keyring from '@polkadot/ui-keyring';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
 
 import { AccountsStore } from '../../stores';
 import Extension from './Extension';
 import State from './State';
+import Tabs from './Tabs';
+import { MetadataDef } from "@polkadot/extension-inject/types";
+import { TypeRegistry } from '@polkadot/types';
+import { defaultExtensions } from '@polkadot/types/extrinsic/signedExtensions';
+import type { SignerPayloadJSON } from '@polkadot/types/types';
 
 describe('Extension', () => {
   let extension: Extension;
+  let state: State;
+  let tabs: Tabs;
   const suri = 'seed sock milk update focus rotate barely fade car face mechanic mercy';
   const password = 'passw0rd';
 
@@ -19,8 +27,9 @@ describe('Extension', () => {
     await cryptoWaitReady();
 
     keyring.loadAll({ store: new AccountsStore() });
-
-    return new Extension(new State());
+    state = new State();
+    tabs = new Tabs(state);
+    return new Extension(state);
   }
 
   const createAccount = async (): Promise<string> => {
@@ -140,6 +149,125 @@ describe('Extension', () => {
       expect(() => {
         pair.decodePkcs8(password);
       }).toThrowError('Unable to decode using the supplied passphrase');
+    });
+  });
+
+  describe('custom user extension', () => {
+    test('Inject metadata', async () => {
+      const types = {
+          PaymentOptions: {
+            feeExchange: "Option<FeeExchange>",
+            tip: "Compact<Balance>"
+          },
+          FeeExchangeV1: {
+            assetId: 'Compact<AssetId>',
+            maxPayment: 'Compact<Balance>',
+          },
+          FeeExchange: {
+            _enum: {
+              FeeExchangeV1: 'FeeExchangeV1'
+            },
+          },
+        }  as unknown as Record<string, string>;
+
+      const userExtensions =
+      {
+        ChargeTransactionPayment: {
+          payload: {},
+          extrinsic: {
+            transactionPayment: 'PaymentOptions',
+          },
+        },
+        CheckEra: {
+          payload: {
+            blockHash: 'Hash',
+          },
+          extrinsic: {
+            era: 'ExtrinsicEra',
+          },
+        },
+        CheckGenesis: {
+          payload: {
+            genesisHash: 'Hash',
+          },
+          extrinsic: {},
+        },
+        CheckNonce: {
+          payload: {},
+          extrinsic: {
+            nonce: 'Compact<Index>',
+          },
+        },
+        CheckSpecVersion: {
+          payload: {
+            specVersion: 'u32',
+          },
+          extrinsic: {},
+        },
+        CheckTxVersion: {
+          payload: {
+            transactionVersion: 'u32',
+          },
+          extrinsic: {},
+        },
+      } as unknown as ExtDef;
+
+      const payload = {
+        address: "14u4eV3nAezEdHQb5zP37P2RtxtBdNHsQBaG69Vk42JiuZg",
+        blockHash: "0xe1b1dda72998846487e4d858909d4f9a6bbd6e338e4588e5d809de16b1317b80",
+        blockNumber: "0x00000393",
+        era: "0x3601",
+        genesisHash: "0xc6b4596042462b51b589a6b467e77671f738c4c011081aa048333be4945528cf",
+        method: "0x040105fa8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4882380100",
+        nonce: "0x0000000000000000",
+        signedExtensions: ["CheckSpecVersion", "CheckTxVersion", "CheckGenesis", "CheckMortality", "CheckNonce", "CheckWeight", "ChargeTransactionPayment"],
+        specVersion: "0x00000026",
+        tip: null,
+        transactionVersion: "0x00000005",
+        version: 4,
+      } as unknown as SignerPayloadJSON;
+
+      const registry = new TypeRegistry();
+      let address = await createAccount();
+      const pair = keyring.getPair(address);
+      pair.decodePkcs8(password);
+
+      const extPayloadWithoutCustomSignedExtension = registry
+        .createType('ExtrinsicPayload', payload, { version: payload.version }).sign(pair);
+      expect(extPayloadWithoutCustomSignedExtension.signature).toEqual('0x003b6675e62d5739c07ca0b0b846ea558d1064c9dd68e81ad86e0a8f92942b348618d90afe45580f2fd850a994d02cd1c3deeba2ad7680c5872508b342362afb0d');
+
+      const meta: MetadataDef = {
+        chain: "Development",
+        genesisHash: "0x242a54b35e1aad38f37b884eddeb71f6f9931b02fac27bf52dfb62ef754e5e62",
+        icon: "",
+        ss58Format: 0,
+        tokenDecimals: 12,
+        tokenSymbol: "",
+        types,
+        userExtensions,
+        color: "#191a2e",
+        specVersion: 38
+      };
+      state.saveMetadata(meta);
+      const allMeta = state.knownMetadata; // get all metadata
+      const metaSavedInState = allMeta.find(metaSaved => metaSaved.genesisHash === meta.genesisHash);
+      expect(metaSavedInState).toEqual(meta);
+
+      registry.setSignedExtensions(defaultExtensions, metaSavedInState?.userExtensions);
+
+      if (metaSavedInState) {
+        registry.register(metaSavedInState?.types);
+      }
+
+      const extPayloadWithCustomSignedExtension =  registry
+        .createType('ExtrinsicPayload', payload, { version: payload.version }).sign(pair);
+      console.log('extPayload:',extPayloadWithCustomSignedExtension);
+      expect(extPayloadWithCustomSignedExtension.signature).toEqual('0x000d1cbc1cd03d5e08b569bda81f13a19557ebcb609170965dc48dd4e4014d315c4d98f07c4944b9f0a1e2fad505376a8ccf0d345a545d19102d111de71cc84903');
+
+      // await expect(tabs.handle('id', 'pub(authorize.tab)', {origin: 'cennznet.io'}, 'http://localhost:3000',{} as chrome.runtime.Port)).resolves.toEqual(true);
+      // await expect(tabs.handle('id', 'pub(extrinsic.sign)', payload, 'http://localhost:3000',{} as chrome.runtime.Port)).resolves.toEqual(true);
+
+
     });
   });
 });
